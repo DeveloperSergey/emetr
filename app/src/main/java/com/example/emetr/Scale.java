f1c6c9c65c93845ed9df294e9c094cc7a9400ac9
Sergei <spb.korolev.ss@gmail.com>
2021-05-25 19:47:42 +0300

    scale workflow.

--- app/src/main/java/com/example/emetr/Scale.java
+++ app/src/main/java/com/example/emetr/Scale.java
@@ -4,6 +4,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.util.Log;
 
 public class Scale extends BasicView{
 
@@ -13,7 +14,7 @@ public class Scale extends BasicView{
         paint.setStrokeWidth(5);
         paint.setColor(Color.DKGRAY);
 
-        int radius = screenParam.height;
+        int radius = screenParam.height - 25;
         int center_x = (int)(screenParam.width/2);
         int center_y = (int)(screenParam.height);
         final RectF oval = new RectF();
@@ -24,15 +25,30 @@ public class Scale extends BasicView{
         int angle = 180 - (2 * screenParam.MIN_ANGLE);
 
         paint.setStrokeWidth(50);
-        paint.setColor(Color.DKGRAY);
+        paint.setColor(Color.LTGRAY);
         canvas.drawArc(oval, startAngle, angle, false, paint);
 
-        paint.setStrokeWidth(10);
-        paint.setColor(Color.GRAY);
-        canvas.drawArc(oval, startAngle, angle, false, paint);
+        // Big
+        paint.setColor(Color.DKGRAY);
+        int arcNum = 20;
+        int arcStart, arcLength;
+        for(int i = 0; i < (arcNum); i++) {
+            arcStart = startAngle + (angle / arcNum) * i;
+            canvas.drawArc(oval, arcStart, (float) 0.2, false, paint);
+        }
+
+        // Little
+        paint.setStrokeWidth(25);
+        radius = screenParam.height - 37;
+        oval.set(center_x - radius, center_y - radius, center_x + radius,
+                center_y + radius);
+        arcNum = 100;
+        for(int i = 0; i < (arcNum); i++) {
+            arcStart = startAngle + (angle / arcNum) * i;
+            canvas.drawArc(oval, arcStart, (float) 0.2, false, paint);
+        }
 
         paint.setStrokeWidth(1);
-        paint.setColor(Color.LTGRAY);
         canvas.drawArc(oval, startAngle, angle, false, paint);
     }
 
